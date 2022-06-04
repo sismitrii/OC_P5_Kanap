b3cd7cf0906f1ddb6fde41b5197fa00fafb9fb57
sismitrii <floguerin156@gmail.com>
2022-06-04 10:58:10 +0200

    🚧 [script.js] : count of product done

--- front/js/script.js
+++ front/js/script.js
@@ -1,2 +1,34 @@
 // pour mettre les programmes commun à toute les pages 
 // comme l'icon au dessus de panier
+
+let productsTab = [];
+let quantityInBag = 0;
+
+function addIconBag() {
+    countQuantityInBag();// faire le compte
+    console.log(quantityInBag);
+    // creer l'icon
+}
+
+function countQuantityInBag(){
+    getTabOfProduct(); // recuperer le tableau
+    count();//compter le total du tableau
+}
+
+function getTabOfProduct(){
+    if (localStorage.kanapProduct !== undefined){
+       productsTab = JSON.parse(localStorage.kanapProduct); 
+    }
+}
+
+function count(){
+    for (let product of productsTab){
+        for (let element in product){
+            if (element != "id"){
+                quantityInBag += product[element];
+            }
+        }
+    }
+}
+
+addIconBag();
