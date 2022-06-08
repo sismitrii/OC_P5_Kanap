38f266df809be91bb80775efb5787ebe6ee568ed
sismitrii <floguerin156@gmail.com>
2022-06-08 16:35:00 +0200

    ✨ [cart.js] : function showAllProductChoiced done

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,5 @@
 
-function showAllProducthoiced(){
+function showAllProductChoiced(){
     let allProductTab = getAllproduct();
     console.log(allProductTab);
     allProductTab.forEach(showProduct);
@@ -27,10 +27,36 @@ function showProduct(product){
 async function createArticle(id, color, qty){
     let productCharacteristic = await getProductCharacteristic(id); // productCharateristic is a promise
     //console.log(id + " : " + color + " : " + qty);
+    console.log(productCharacteristic);
+
+    const cartTag = document.getElementById('cart__items');
+
     let article = document.createElement('article');
     article.classList.add('cart__item');
     article.dataset.id = id;
     article.dataset.color = color;
+    article.innerHTML = `<div class="cart__item__img">
+    <img src="${productCharacteristic.imageUrl}" alt="${productCharacteristic.altTxt}">
+  </div>
+  <div class="cart__item__content">
+    <div class="cart__item__content__description">
+      <h2>${productCharacteristic.name}</h2>
+      <p>${color}</p>
+      <p>${productCharacteristic.price} €</p>
+    </div>
+    <div class="cart__item__content__settings">
+      <div class="cart__item__content__settings__quantity">
+        <p>Qté : </p>
+        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${qty}">
+      </div>
+      <div class="cart__item__content__settings__delete">
+        <p class="deleteItem">Supprimer</p>
+      </div>
+    </div>
+  </div>`;
+
+  cartTag.appendChild(article);
+
 }
 
 function getProductCharacteristic(productID){
@@ -51,4 +77,4 @@ function getProductCharacteristic(productID){
         });
 }
 
-showAllProducthoiced();
+showAllProductChoiced();
