71a09d98ec0f3333393796bd4fb90b656885069f
sismitrii <floguerin156@gmail.com>
2022-06-14 06:59:18 +0200

    ✨ [confimation.js] : orderId added to the DOM

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,1 +1,15 @@
-// il faut recupérer avec URL Param' l'orderId et le mettre dans le span orderId
+let orderId;
+
+function getProductIdInUrl(){
+    let url = new URL(window.location.href);
+    orderId = url.searchParams.get('orderID');
+}
+
+function showOrderId(){
+    getProductIdInUrl();
+    const orderIDTag = document.getElementById('orderId');
+    orderIDTag.innerHTML = `<br/> ${orderId}`;
+    orderIDTag.style = "font-weight : bold";
+}
+
+showOrderId();
